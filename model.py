770f0c4723f2f120c82a9094bdaf260e419537a6
Tien <viettienha98@gmail.com>
2020-02-03 13:28:39 +0700

    First commit

---
+++ model.py
@@ -0,0 +1,55 @@
+import torch
+from torch import nn
+
+
+class FruitNet(nn.Module):
+    def __init__(self, class_num):
+        super(FruitNet, self).__init__()
+        # input size: 100x100x3
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=(5, 5))
+        self.relu1 = nn.ReLU()
+        self.pool1 = nn.AvgPool2d(kernel_size=(2, 2), stride=2)
+        # output: 48, 48, 16
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=(5, 5))
+        self.relu2 = nn.ReLU()
+        self.pool2 = nn.AvgPool2d(kernel_size=(2, 2), stride=2)
+        # output: 50, 50, 16
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=(5, 5))
+        self.relu3 = nn.ReLU()
+        self.pool3 = nn.AvgPool2d(kernel_size=(2, 2), stride=2)
+        self.conv4 = nn.Conv2d(64, 128, kernel_size=(5, 5))
+        self.relu4 = nn.ReLU()
+        self.pool4 = nn.AvgPool2d(kernel_size=(2, 2), stride=2)
+        # m1 = [batch_size x in_features] || m2 = [in_features x out_features]
+        # input feature: C x H x W
+        self.fc1 = nn.Linear(128*2*2, 256)
+        self.relu5 = nn.ReLU()
+        self.fc2 = nn.Linear(256, class_num)
+        self.sfmx = nn.Softmax(dim=1)
+
+    def forward(self, input):
+        x = self.conv1(input)
+        x = self.relu1(x)
+        x = self.pool1(x)
+        x = self.conv2(x)
+        x = self.relu2(x)
+        x = self.pool2(x)
+        x = self.conv3(x)
+        x = self.relu3(x)
+        x = self.pool3(x)
+        x = self.conv4(x)
+        x = self.relu4(x)
+        x = self.pool4(x)
+        # We need to reshape it into [batch_size x tensor]
+        x = x.view(x.shape[0], -1)
+        x = self.fc1(x)
+        x = self.relu5(x)
+        x = self.fc2(x)
+        x = self.sfmx(x)
+        return x
+
+
+if __name__ == "__main__":
+    input = torch.ones([2, 3, 100, 100], dtype=torch.float32)
+    model = FruitNet()
+    print(model(input).shape)
---
+++ train.py
@@ -0,0 +1,118 @@
+import torch
+import torchvision
+from torchvision.datasets import ImageFolder
+from torchvision import transforms
+from torch.utils.data import DataLoader
+import torch.optim as optim
+from model import FruitNet
+import argparse
+
+parser = argparse.ArgumentParser(
+    description='Simple training script ')
+
+parser.add_argument('--epochs', help='Number of epochs',
+                    type=int, default=20)
+
+
+parser.add_argument('--batch_size', default=32, type=int,
+                    help='Batch size for training')
+
+parser.add_argument('--train', type=str, default='fruits_data/Training',
+                    help='Rootdir of train data')
+
+parser.add_argument('--val', type=str, default='fruits_data/Test',
+                    help='Rootdir of val data')
+args = parser.parse_args()
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+def main():
+    # flatten dataset
+    tfms = transforms.Compose([
+        transforms.Resize((100, 100)),
+        transforms.ToTensor(),
+        # transforms.Normalize(
+        #     [0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+    train_dataset = ImageFolder(args.train, transform=tfms)
+    train_dataloader = DataLoader(
+        train_dataset, batch_size=args.batch_size, shuffle=True)
+    val_dataset = ImageFolder(args.val, transform=tfms)
+    val_dataloader = DataLoader(
+        val_dataset, batch_size=args.batch_size, shuffle=True)
+
+    # param
+    model = FruitNet(len(train_dataset.classes)).to(device)
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.0015)
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(
+        optimizer, mode='min', factor=0.1, patience=4)
+
+    # Train
+    print('Start training')
+    for epoch in range(args.epochs):
+        print('Epoch [{}/{}]:'.format(epoch+1, args.epochs))
+        model.train()
+        with torch.enable_grad():
+            j = 0
+            running_loss = 0.0
+            correct = 0
+            total = 0
+            for i, (images, labels) in enumerate(train_dataloader):
+                print(".", end='')
+                images = images.to(device)
+                labels = labels.to(device)
+                # print('Image: {} - label: {}'.format(images.shape, labels))
+                # Forward pass
+                outputs = model(images)
+                # print('Output:{}'.format(outputs))
+                loss = criterion(outputs, labels)
+
+                # Loss and Acc
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                # .item() won't save full architecture
+                correct += (predicted == labels).sum().item()
+                running_loss += loss.item() * images.size(0)
+                j = j+1
+
+                # Backward and optimize
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+        print('\nTrain Loss: {:.4f}, Train Acc: {:.2f}'.format(running_loss /
+                                                               (j*args.batch_size),
+                                                               correct/total))
+
+        # Eval mode
+        model.eval()
+        with torch.no_grad():
+            j = 0
+            running_loss = 0.0
+            correct = 0
+            total = 0
+            for i, (images, labels) in enumerate(val_dataloader):
+                print(".", end='')
+                images = images.to(device)
+                labels = labels.to(device)
+                # print('Val:')
+                # print('Image: {} - label: {}'.format(images.shape, labels))
+                # Forward pass
+                outputs = model(images)
+                # print('Output:{}'.format(outputs))
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+                running_loss += loss.item() * images.size(0)
+                j = j+1
+
+        print('\nVal Loss: {:.4f}, Val Acc: {:.2f}'.format(running_loss / (j*args.batch_size),
+                                                           correct/total))
+        # Check val loss
+        scheduler.step(running_loss)
+
+
+if __name__ == '__main__':
+    main()
